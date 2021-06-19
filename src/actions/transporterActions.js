373589cc636627d3cf0d2a4811afd60c8eced678
MeryAmun <chebesiawah@gmail.com>
2021-06-19 03:37:13 +0100

    transporter action and reducer created

--- src/actions/transporterActions.js
+++ src/actions/transporterActions.js
@@ -1,5 +1,5 @@
 import {
-    TRANSPORTER_LIST_REQUEST, TRANSPORTER_LIST_SUCCESS, TRANSPORTER_LIST_FAIL, TRANSPORTER_DETAILS_REQUEST, TRANSPORTER_DETAILS_SUCCESS, TRANSPORTER_DETAILS_FAIL}
+    TRANSPORTER_LIST_REQUEST, TRANSPORTER_LIST_SUCCESS, TRANSPORTER_LIST_FAIL, TRANSPORTER_DETAILS_REQUEST, TRANSPORTER_DETAILS_SUCCESS, TRANSPORTER_DETAILS_FAIL,TRANSPORTER_SAVE_REQUEST, TRANSPORTER_SAVE_SUCCESS, TRANSPORTER_SAVE_FAIL}
 from '../constants/transporterConstant'
 import axios from 'axios'
 
@@ -16,6 +16,27 @@ const listTransporters = () => async (dispatch) => {
        dispatch({ type: TRANSPORTER_LIST_FAIL, payload: error.message });
     }
    }
+
+
+   //Save transporter action..to db
+ 
+ const saveTransporter = (transporter) => async (dispatch, getState) => {
+   try {
+      dispatch({ type: TRANSPORTER_SAVE_REQUEST, payload: transporter });
+      const { userSignin: { userInfo } } = getState();
+      const { data } = await Axios.post('http://127.0.0.1:3500/api/transporters', transporter, {
+         headers: {
+            'Authorization': 'Bearer' + userInfo.token
+         }
+      });
+      dispatch({ type: TRANSPORTER_SAVE_SUCCESS, payload: data });
+
+   } catch (error) {
+      dispatch({ type: TRANSPORTER_SAVE_FAIL, payload: error.message });
+   }
+}
+
+
 //transporters details action
 const detailsTransporter = (transporterId) => async (dispatch) => {
  
@@ -30,4 +51,4 @@ const detailsTransporter = (transporterId) => async (dispatch) => {
 }
 
 
-export { listTransporters, detailsTransporter }
+export { listTransporters, detailsTransporter, saveTransporter }
